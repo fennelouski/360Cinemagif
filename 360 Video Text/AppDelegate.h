97fd76f1bb3daa381825c0fffaf70fffb839cbc4
Nathan Fennel <nathanfennel@gmail.com>
2015-10-17 14:21:36 -0400

    Initial Commit

---
+++ 360 Video Text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  360 Video Text
+//
+//  Created by Nathan Fennel on 10/17/15.
+//  Copyright © 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 360 Video Text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  360 Video Text
+//
+//  Created by Nathan Fennel on 10/17/15.
+//  Copyright © 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

6c79ea8a3fdcb1d35aa62d39fda44a30d460fae8
Nathan Fennel <nathanfennel@gmail.com>
2015-10-18 15:05:03 -0400

    Added View Controller In front of video recorder

---
+++ 360 Video Text/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  360 Video Text
+//
+//  Created by Nathan Fennel on 10/18/15.
+//  Copyright © 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end

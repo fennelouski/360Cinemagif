14081eedbadde989ca988f4699b1dd49b30e6391
Nathan Fennel <nathanfennel@gmail.com>
2015-10-21 11:15:20 -0400

    Dynamically set the length of video based on device

--- 360 Video Text/VRDataManager.m
+++ 360 Video Text/VRDataManager.m
@@ -15,6 +15,7 @@
     CGSize frameSize;
 	CGSize exportingFrameSize;
 	float exportingRatio;
+	int batchSize;
 }
 
 + (instancetype)sharedManager {
@@ -33,6 +34,7 @@
 	
 	if (self) {
 		exportingRatio = [self outputRatio];
+		batchSize = [self batchSize];
 	}
 	
 	return self;
@@ -62,7 +64,7 @@
 }
 
 + (int)numberOfImagesPerBatch {
-    return 300;
+    return [[VRDataManager sharedManager] batchSize];
 }
 
 
@@ -125,4 +127,51 @@
 	return 0.1562f;
 }
 
+- (int)batchSize {
+	NSString *platform = [self platform];
+	
+	if ([platform isEqualToString:@"iPhone1,1"])    return 5; //@"iPhone 1G";
+	if ([platform isEqualToString:@"iPhone1,2"])    return 5; //@"iPhone 3G";
+	if ([platform isEqualToString:@"iPhone2,1"])    return 5; //@"iPhone 3GS";
+	if ([platform isEqualToString:@"iPhone3,1"])    return 15; //@"iPhone 4";
+	if ([platform isEqualToString:@"iPhone3,3"])    return 30; //@"Verizon iPhone 4";
+	if ([platform isEqualToString:@"iPhone4,1"])    return 60; //@"iPhone 4S";
+	if ([platform isEqualToString:@"iPhone5,1"])    return 120; //@"iPhone 5 (GSM)";
+	if ([platform isEqualToString:@"iPhone5,2"])    return 120; //@"iPhone 5 (GSM+CDMA)";
+	if ([platform isEqualToString:@"iPhone5,3"])    return 120; //@"iPhone 5c (GSM)";
+	if ([platform isEqualToString:@"iPhone5,4"])    return 120; //@"iPhone 5c (GSM+CDMA)";
+	if ([platform isEqualToString:@"iPhone6,1"])    return 240; //@"iPhone 5s (GSM)";
+	if ([platform isEqualToString:@"iPhone6,2"])    return 240; //@"iPhone 5s (GSM+CDMA)";
+	if ([platform isEqualToString:@"iPhone7,1"])    return 300; //@"iPhone 6 Plus";
+	if ([platform isEqualToString:@"iPhone7,2"])    return 300; //@"iPhone 6";
+	if ([platform isEqualToString:@"iPhone8,1"])    return 300; //@"iPhone 6s Plus";
+	if ([platform isEqualToString:@"iPhone8,2"])    return 300; //@"iPhone 6s";
+	if ([platform isEqualToString:@"iPod1,1"])      return 5; //@"iPod Touch 1G";
+	if ([platform isEqualToString:@"iPod2,1"])      return 5; //@"iPod Touch 2G";
+	if ([platform isEqualToString:@"iPod3,1"])      return 5; //@"iPod Touch 3G";
+	if ([platform isEqualToString:@"iPod4,1"])      return 5; //@"iPod Touch 4G";
+	if ([platform isEqualToString:@"iPod5,1"])      return 5; //@"iPod Touch 5G";
+	if ([platform isEqualToString:@"iPad1,1"])      return 240; //@"iPad";
+	if ([platform isEqualToString:@"iPad2,1"])      return 240; //@"iPad 2 (WiFi)";
+	if ([platform isEqualToString:@"iPad2,2"])      return 240; //@"iPad 2 (GSM)";
+	if ([platform isEqualToString:@"iPad2,3"])      return 240; //@"iPad 2 (CDMA)";
+	if ([platform isEqualToString:@"iPad2,4"])      return 240; //@"iPad 2 (WiFi)";
+	if ([platform isEqualToString:@"iPad2,5"])      return 240; //@"iPad Mini (WiFi)";
+	if ([platform isEqualToString:@"iPad2,6"])      return 240; //@"iPad Mini (GSM)";
+	if ([platform isEqualToString:@"iPad2,7"])      return 240; //@"iPad Mini (GSM+CDMA)";
+	if ([platform isEqualToString:@"iPad3,1"])      return 240; //@"iPad 3 (WiFi)";
+	if ([platform isEqualToString:@"iPad3,2"])      return 240; //@"iPad 3 (GSM+CDMA)";
+	if ([platform isEqualToString:@"iPad3,3"])      return 240; //@"iPad 3 (GSM)";
+	if ([platform isEqualToString:@"iPad3,4"])      return 240; //@"iPad 4 (WiFi)";
+	if ([platform isEqualToString:@"iPad3,5"])      return 240; //@"iPad 4 (GSM)";
+	if ([platform isEqualToString:@"iPad3,6"])      return 240; //@"iPad 4 (GSM+CDMA)";
+	if ([platform isEqualToString:@"iPad4,1"])      return 240; //@"iPad Air (WiFi)";
+	if ([platform isEqualToString:@"iPad4,2"])      return 240; //@"iPad Air (Cellular)";
+	if ([platform isEqualToString:@"iPad4,4"])      return 240; //@"iPad mini 2G (WiFi)";
+	if ([platform isEqualToString:@"iPad4,5"])      return 240; //@"iPad mini 2G (Cellular)";
+	if ([platform isEqualToString:@"i386"])         return 120; //@"Simulator";
+	if ([platform isEqualToString:@"x86_64"])       return 240; //@"Simulator";
+	
+	return 0.1562f;
+}
 @end
